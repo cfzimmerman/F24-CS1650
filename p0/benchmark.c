c8f980d6b1f5b571db94401e0fc77a91c1e91de2
cfzimmerman <coryzimmerman93@gmail.com>
2024-09-08 15:14:27 -0400

    extended benchmark 9 - 10 secs

--- p0/benchmark.c
+++ p0/benchmark.c
@@ -14,28 +14,48 @@
 int main(void) {
 
   HashTable *ht = NULL;
-  int num_tests = 50000000;
-  assert(allocate(&ht, num_tests) == 0);
+  const int NUM_TESTS = 50000000;
+  int *keys = malloc(NUM_TESTS * sizeof(int));
+  assert(keys != NULL);
+
+  assert(allocate(&ht, NUM_TESTS) == 0);
 
   int seed = 2;
   srand(seed);
-  printf("Performing stress test. Inserting 50 million keys.\n");
+  printf("Performing stress test. Inserting, getting, and erasing %d "
+         "keys.\n",
+         NUM_TESTS);
 
   struct timeval stop, start;
   gettimeofday(&start, NULL);
 
-  for (int i = 0; i < num_tests; i += 1) {
+  for (int i = 0; i < NUM_TESTS; i += 1) {
     int key = rand();
     int val = rand();
+    keys[i] = key;
     assert(put(ht, key, val) == 0);
+  }
+
+  printf("Finished 'put', starting 'get'\n");
+  const int NUM_VALS = 10;
+  ValType vals[NUM_VALS];
+  for (int i = 0; i < NUM_TESTS; i += 1) {
+    int num_results = 0;
+    get(ht, keys[i], vals, NUM_VALS, &num_results);
+  }
+
+  printf("Finished 'get', starting 'erase'\n");
+  for (int i = 0; i < NUM_TESTS; i += 1) {
+    erase(ht, keys[i]);
   }
 
   gettimeofday(&stop, NULL);
   double secs = (double)(stop.tv_usec - start.tv_usec) / 1000000 +
                 (double)(stop.tv_sec - start.tv_sec);
-  printf("50 million insertions took %f seconds\n", secs);
+  printf("Took %f seconds\n", secs);
 
   assert(deallocate(ht) == 0);
+  free(keys);
 
   return 0;
 }
